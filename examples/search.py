379f619ac1b001e2298c8857075536146443b88c
Victor M. Alvarez <vmalvarez@virustotal.com>
2019-07-01 10:45:40 +0200

    Add search example.

---
+++ examples/search.py
@@ -0,0 +1,59 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+# Copyright © 2019 The vt-py authors. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This example program shows how to make VirusTotal Intelligence searches, the
+program accepts a query a prints the matching files/URLs.
+
+NOTICE: In order to use this program you will need an API key that has
+privileges for using VirusTotal Intelligence.
+"""
+
+import argparse
+import vt
+
+
+def main():
+
+  parser = argparse.ArgumentParser(
+      description='Make a VirusTotal Intelligence search and prints the matching objects.')
+
+  parser.add_argument('query',
+      type=str,
+      nargs='+',
+      help='a VirusTotal Intelligence search query.')
+
+  parser.add_argument('--apikey',
+      required=True,
+      help='your VirusTotal API key')
+
+  parser.add_argument('--limit',
+      type=int,
+      required=False,
+      help='maximum number of objects that will be retrieved',
+      default=50)
+
+  args = parser.parse_args()
+
+  with vt.Client(args.apikey) as client:
+    it = client.iterator('/intelligence/search',
+        params={'query': ' '.join(args.query)},
+        limit=args.limit)
+    for obj in it:
+      print("{}:{}".format(obj.type, obj.id))
+
+
+if __name__ == '__main__':
+  main()

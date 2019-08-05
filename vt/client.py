VirusTotal/vt-py

commit c8f7521f40142e845fc3c610580681f2ddfe01ac

Accept placeholders in path passed to client.iterator()

vt/client.py
@@ -498,6 +498,8 @@ class Client:
     a collection, like /intelligence/hunting_rulesets.
 
     :param path: Path to API endpoint.
+    :param fmt_args: A variable number of arguments that are put into any
+      placeholders used in path.
     :param obj: Instance :class:`Object` whith the type expected by the API
       endpoint.
     :type path: str
@@ -512,7 +514,7 @@ class Client:
     response = await self.post_async(path, *fmt_args, data=data)
     return await self._response_to_object(response)
 
-  def iterator(self, path, params=None, cursor=None,
+  def iterator(self, path, *fmt_args, params=None, cursor=None,
                limit=None, batch_size=None):
     """Returns an iterator for the collection specified by the given path.
 
@@ -520,6 +522,8 @@ class Client:
     example of such an endpoint are /comments and /intelligence/search.
 
     :param path: Path to API endpoint returning a collection.
+    :param fmt_args: A variable number of arguments that are put into any
+      placeholders used in path.
     :param params: Additional parameters passed to the endpoint.
     :param cursor: Cursor for resuming the iteration at the point it was left
       previously. A cursor can be obtained with Iterator.cursor(). This

tests/test_client.py
@@ -233,7 +233,7 @@ def test_delete(httpserver):
 def test_iterator(httpserver):
 
   httpserver.expect_request(
-      '/api/v3/dummy_collection',
+      '/api/v3/dummy_collection/foo',
       method='GET',
       headers={'X-Apikey': 'dummy_api_key'}
   ).respond_with_json({
@@ -245,9 +245,8 @@ def test_iterator(httpserver):
   })
 
   with new_client(httpserver) as client:
-    it = client.iterator('/dummy_collection')
+    it = client.iterator('/dummy_collection/{}', 'foo')
     for i, obj in enumerate(it):
-      print(obj.id)
       assert 0 == i
 
 

commit ad2c8d8fdb902ad0009940768ce648fce2268e0f

Fix issue introduced in c8f7521f40142e845fc3c610580681f2ddfe01ac

vt/client.py
@@ -541,11 +541,8 @@ class Client:
     :type batch_size: int
     :returns: An instance of :class:`Iterator`.
     """
-    return Iterator(self, path,
-        params=params,
-        cursor=cursor,
-        limit=limit,
-        batch_size=batch_size)
+    return Iterator(self, self._full_url(path, *path_args),
+        params=params, cursor=cursor, limit=limit, batch_size=batch_size)
 
   def scan_file(self, file, wait_for_completion=False):
     """Scans a file.
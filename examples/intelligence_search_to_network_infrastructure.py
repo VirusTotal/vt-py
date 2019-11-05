272904d3c19cb68b69014f8e38082837d61af225
Emiliano Martinez <emilianomart@google.com>
2019-11-05 12:46:34 +0100

    Remove python3 linting comment

--- examples/intelligence_search_to_network_infrastructure.py
+++ examples/intelligence_search_to_network_infrastructure.py
@@ -1,5 +1,4 @@
-# Lint as: python3
-
+#!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 # Copyright © 2019 The vt-py authors. All Rights Reserved.
 # Licensed under the Apache License, Version 2.0 (the "License");

55d014c7112658aeadd70221ef29f7c38940b44c
Shen Qin <shen.qin@outlook.sg>
2023-08-11 22:36:46 +0800

    Modified wait_for_analysis_completion and reverted changes

--- examples/upload_files.py
+++ examples/upload_files.py
@@ -55,9 +55,8 @@ async def upload_hashes(queue, apikey):
 async def process_analysis_results(apikey, analysis, file_path):
   async with vt.Client(apikey) as client:
     completed_analysis = await client.wait_for_analysis_completion(analysis)
-    analysis_result = await completed_analysis
-    print(f'{file_path}: {analysis_result.stats}')
-    print(f'analysis_id: {analysis_result.id}')
+    print(f'{file_path}: {completed_analysis.stats}')
+    print(f'analysis id: {completed_analysis.id}')
 
 
 async def main():
